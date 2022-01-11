7ad64eaf40d291f8ee4579d8de2a3660dbda18b9
Siddharth Bhatia <32610387+Sid-Bhatia-0@users.noreply.github.com>
2022-01-11 19:16:13 +0530

    add fully integer-based ray casting (#5)
    * add integer based cast_ray method
    
    * cleanup integer-based cast_ray method

--- src/RayCaster.jl
+++ src/RayCaster.jl
@@ -48,4 +48,53 @@ function cast_ray(obstacle_tile_map::AbstractArray{Bool, 2}, x_start::T1, y_star
     return i_stop, j_stop, hit_dimension, total_euclidean
 end
 
+function cast_ray(obstacle_tile_map::AbstractArray{Bool, 2}, i_start_world_units, j_start_world_units, delta_i_world_units, delta_j_world_units, world_units_per_tile_unit)
+    I = typeof(i_start_world_units)
+
+    i_start_tile_units = i_start_world_units ÷ world_units_per_tile_unit + one(I)
+    j_start_tile_units = j_start_world_units ÷ world_units_per_tile_unit + one(I)
+
+    delta_euclidean_per_world_unit_i = abs(delta_j_world_units)
+    delta_euclidean_per_world_unit_j = abs(delta_i_world_units)
+
+    delta_euclidean_per_tile_unit_i = world_units_per_tile_unit * delta_euclidean_per_world_unit_i
+    delta_euclidean_per_tile_unit_j = world_units_per_tile_unit * delta_euclidean_per_world_unit_j
+
+    if delta_i_world_units < zero(I)
+        delta_i_tile_units = -one(I)
+        delta_euclidean_i = (i_start_world_units - (i_start_tile_units - one(I)) * world_units_per_tile_unit) * delta_euclidean_per_world_unit_i
+    else
+        delta_i_tile_units = one(I)
+        delta_euclidean_i = (i_start_tile_units * world_units_per_tile_unit - i_start_world_units + one(I)) * delta_euclidean_per_world_unit_i
+    end
+
+    if delta_j_world_units < zero(I)
+        delta_j_tile_units = -one(I)
+        delta_euclidean_j = (j_start_world_units - (j_start_tile_units - one(I)) * world_units_per_tile_unit) * delta_euclidean_per_world_unit_j
+    else
+        delta_j_tile_units = one(I)
+        delta_euclidean_j = (j_start_tile_units * world_units_per_tile_unit - j_start_world_units + one(I)) * delta_euclidean_per_world_unit_j
+    end
+
+    i_stop_tile_units = i_start_tile_units
+    j_stop_tile_units = j_start_tile_units
+    hit_dimension = 1
+
+    while !obstacle_tile_map[i_stop_tile_units, j_stop_tile_units]
+
+        if (delta_euclidean_i <= delta_euclidean_j)
+            delta_euclidean_i += delta_euclidean_per_tile_unit_i
+            i_stop_tile_units += delta_i_tile_units
+            hit_dimension = 1
+        else
+            delta_euclidean_j += delta_euclidean_per_tile_unit_j
+            j_stop_tile_units += delta_j_tile_units
+            hit_dimension = 2
+        end
+
+    end
+
+    return i_stop_tile_units, j_stop_tile_units, hit_dimension
+end
+
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -10,6 +10,9 @@ Test.@testset "RayCaster.jl" begin
                                   1 1 1 1 1
                                  ])
 
+    height_obstacle_tile_map = size(obstacle_tile_map, 1)
+    width_obstacle_tile_map = size(obstacle_tile_map, 2)
+
     T1 = Float32
     T2 = Float32
 
@@ -146,5 +149,184 @@ Test.@testset "RayCaster.jl" begin
         Test.@test j_stop == 2
         Test.@test hit_dimension == 1
         Test.@test total_euclidean ≈ sqrt(1.5 ^ 2 + 1 ^ 2)
+    end
+
+    I = Int
+    world_units_per_tile_unit = convert(I, 256)
+
+    Test.@testset "delta_i = 1, delta_j = 0" begin
+        i_start_world_units = convert(I, height_obstacle_tile_map * world_units_per_tile_unit ÷ 2 + 1)
+        j_start_world_units = convert(I, width_obstacle_tile_map * world_units_per_tile_unit ÷ 2 + 1)
+        delta_i_world_units = convert(I, 1)
+        delta_j_world_units = convert(I, 0)
+        i_stop_tile_units, j_stop_tile_units, hit_dimension = RC.cast_ray(obstacle_tile_map, i_start_world_units, j_start_world_units, delta_i_world_units, delta_j_world_units, world_units_per_tile_unit)
+        Test.@test i_stop_tile_units == convert(I, 5)
+        Test.@test j_stop_tile_units == convert(I, 3)
+        Test.@test hit_dimension == 1
+    end
+
+    Test.@testset "delta_i = 2, delta_j = 1" begin
+        i_start_world_units = convert(I, height_obstacle_tile_map * world_units_per_tile_unit ÷ 2 + 1)
+        j_start_world_units = convert(I, width_obstacle_tile_map * world_units_per_tile_unit ÷ 2 + 1)
+        delta_i_world_units = convert(I, 2)
+        delta_j_world_units = convert(I, 1)
+        i_stop_tile_units, j_stop_tile_units, hit_dimension = RC.cast_ray(obstacle_tile_map, i_start_world_units, j_start_world_units, delta_i_world_units, delta_j_world_units, world_units_per_tile_unit)
+        Test.@test i_stop_tile_units == convert(I, 5)
+        Test.@test j_stop_tile_units == convert(I, 4)
+        Test.@test hit_dimension == 1
+    end
+
+    Test.@testset "delta_i = 1, delta_j = 1" begin
+        i_start_world_units = convert(I, height_obstacle_tile_map * world_units_per_tile_unit ÷ 2 + 1)
+        j_start_world_units = convert(I, width_obstacle_tile_map * world_units_per_tile_unit ÷ 2 + 1)
+        delta_i_world_units = convert(I, 1)
+        delta_j_world_units = convert(I, 1)
+        i_stop_tile_units, j_stop_tile_units, hit_dimension = RC.cast_ray(obstacle_tile_map, i_start_world_units, j_start_world_units, delta_i_world_units, delta_j_world_units, world_units_per_tile_unit)
+        Test.@test i_stop_tile_units == convert(I, 5)
+        Test.@test j_stop_tile_units == convert(I, 4)
+        Test.@test hit_dimension == 1
+    end
+
+    Test.@testset "delta_i = 1, delta_j = 2" begin
+        i_start_world_units = convert(I, height_obstacle_tile_map * world_units_per_tile_unit ÷ 2 + 1)
+        j_start_world_units = convert(I, width_obstacle_tile_map * world_units_per_tile_unit ÷ 2 + 1)
+        delta_i_world_units = convert(I, 1)
+        delta_j_world_units = convert(I, 2)
+        i_stop_tile_units, j_stop_tile_units, hit_dimension = RC.cast_ray(obstacle_tile_map, i_start_world_units, j_start_world_units, delta_i_world_units, delta_j_world_units, world_units_per_tile_unit)
+        Test.@test i_stop_tile_units == convert(I, 4)
+        Test.@test j_stop_tile_units == convert(I, 5)
+        Test.@test hit_dimension == 2
+    end
+
+    Test.@testset "delta_i = 0, delta_j = 1" begin
+        i_start_world_units = convert(I, height_obstacle_tile_map * world_units_per_tile_unit ÷ 2 + 1)
+        j_start_world_units = convert(I, width_obstacle_tile_map * world_units_per_tile_unit ÷ 2 + 1)
+        delta_i_world_units = convert(I, 0)
+        delta_j_world_units = convert(I, 1)
+        i_stop_tile_units, j_stop_tile_units, hit_dimension = RC.cast_ray(obstacle_tile_map, i_start_world_units, j_start_world_units, delta_i_world_units, delta_j_world_units, world_units_per_tile_unit)
+        Test.@test i_stop_tile_units == convert(I, 3)
+        Test.@test j_stop_tile_units == convert(I, 5)
+        Test.@test hit_dimension == 2
+    end
+
+    Test.@testset "delta_i = -1, delta_j = 2" begin
+        i_start_world_units = convert(I, height_obstacle_tile_map * world_units_per_tile_unit ÷ 2 + 1)
+        j_start_world_units = convert(I, width_obstacle_tile_map * world_units_per_tile_unit ÷ 2 + 1)
+        delta_i_world_units = convert(I, -1)
+        delta_j_world_units = convert(I, 2)
+        i_stop_tile_units, j_stop_tile_units, hit_dimension = RC.cast_ray(obstacle_tile_map, i_start_world_units, j_start_world_units, delta_i_world_units, delta_j_world_units, world_units_per_tile_unit)
+        Test.@test i_stop_tile_units == convert(I, 2)
+        Test.@test j_stop_tile_units == convert(I, 5)
+        Test.@test hit_dimension == 2
+    end
+
+    Test.@testset "delta_i = -1, delta_j = 1" begin
+        i_start_world_units = convert(I, height_obstacle_tile_map * world_units_per_tile_unit ÷ 2 + 1)
+        j_start_world_units = convert(I, width_obstacle_tile_map * world_units_per_tile_unit ÷ 2 + 1)
+        delta_i_world_units = convert(I, -1)
+        delta_j_world_units = convert(I, 1)
+        i_stop_tile_units, j_stop_tile_units, hit_dimension = RC.cast_ray(obstacle_tile_map, i_start_world_units, j_start_world_units, delta_i_world_units, delta_j_world_units, world_units_per_tile_unit)
+        Test.@test i_stop_tile_units == convert(I, 2)
+        Test.@test j_stop_tile_units == convert(I, 5)
+        Test.@test hit_dimension == 2
+    end
+
+    Test.@testset "delta_i = -2, delta_j = 1" begin
+        i_start_world_units = convert(I, height_obstacle_tile_map * world_units_per_tile_unit ÷ 2 + 1)
+        j_start_world_units = convert(I, width_obstacle_tile_map * world_units_per_tile_unit ÷ 2 + 1)
+        delta_i_world_units = convert(I, -2)
+        delta_j_world_units = convert(I, 1)
+        i_stop_tile_units, j_stop_tile_units, hit_dimension = RC.cast_ray(obstacle_tile_map, i_start_world_units, j_start_world_units, delta_i_world_units, delta_j_world_units, world_units_per_tile_unit)
+        Test.@test i_stop_tile_units == convert(I, 1)
+        Test.@test j_stop_tile_units == convert(I, 4)
+        Test.@test hit_dimension == 1
+    end
+
+    Test.@testset "delta_i = -1, delta_j = 0" begin
+        i_start_world_units = convert(I, height_obstacle_tile_map * world_units_per_tile_unit ÷ 2 + 1)
+        j_start_world_units = convert(I, width_obstacle_tile_map * world_units_per_tile_unit ÷ 2 + 1)
+        delta_i_world_units = convert(I, -1)
+        delta_j_world_units = convert(I, 0)
+        i_stop_tile_units, j_stop_tile_units, hit_dimension = RC.cast_ray(obstacle_tile_map, i_start_world_units, j_start_world_units, delta_i_world_units, delta_j_world_units, world_units_per_tile_unit)
+        Test.@test i_stop_tile_units == convert(I, 1)
+        Test.@test j_stop_tile_units == convert(I, 3)
+        Test.@test hit_dimension == 1
+    end
+
+    Test.@testset "delta_i = -2, delta_j = -1" begin
+        i_start_world_units = convert(I, height_obstacle_tile_map * world_units_per_tile_unit ÷ 2 + 1)
+        j_start_world_units = convert(I, width_obstacle_tile_map * world_units_per_tile_unit ÷ 2 + 1)
+        delta_i_world_units = convert(I, -2)
+        delta_j_world_units = convert(I, -1)
+        i_stop_tile_units, j_stop_tile_units, hit_dimension = RC.cast_ray(obstacle_tile_map, i_start_world_units, j_start_world_units, delta_i_world_units, delta_j_world_units, world_units_per_tile_unit)
+        Test.@test i_stop_tile_units == convert(I, 1)
+        Test.@test j_stop_tile_units == convert(I, 2)
+        Test.@test hit_dimension == 1
+    end
+
+    Test.@testset "delta_i = -1, delta_j = -1" begin
+        i_start_world_units = convert(I, height_obstacle_tile_map * world_units_per_tile_unit ÷ 2 + 1)
+        j_start_world_units = convert(I, width_obstacle_tile_map * world_units_per_tile_unit ÷ 2 + 1)
+        delta_i_world_units = convert(I, -1)
+        delta_j_world_units = convert(I, -1)
+        i_stop_tile_units, j_stop_tile_units, hit_dimension = RC.cast_ray(obstacle_tile_map, i_start_world_units, j_start_world_units, delta_i_world_units, delta_j_world_units, world_units_per_tile_unit)
+        Test.@test i_stop_tile_units == convert(I, 1)
+        Test.@test j_stop_tile_units == convert(I, 2)
+        Test.@test hit_dimension == 1
+    end
+
+    Test.@testset "delta_i = -1, delta_j = -2" begin
+        i_start_world_units = convert(I, height_obstacle_tile_map * world_units_per_tile_unit ÷ 2 + 1)
+        j_start_world_units = convert(I, width_obstacle_tile_map * world_units_per_tile_unit ÷ 2 + 1)
+        delta_i_world_units = convert(I, -1)
+        delta_j_world_units = convert(I, -2)
+        i_stop_tile_units, j_stop_tile_units, hit_dimension = RC.cast_ray(obstacle_tile_map, i_start_world_units, j_start_world_units, delta_i_world_units, delta_j_world_units, world_units_per_tile_unit)
+        Test.@test i_stop_tile_units == convert(I, 2)
+        Test.@test j_stop_tile_units == convert(I, 1)
+        Test.@test hit_dimension == 2
+    end
+
+    Test.@testset "delta_i = 0, delta_j = -1" begin
+        i_start_world_units = convert(I, height_obstacle_tile_map * world_units_per_tile_unit ÷ 2 + 1)
+        j_start_world_units = convert(I, width_obstacle_tile_map * world_units_per_tile_unit ÷ 2 + 1)
+        delta_i_world_units = convert(I, 0)
+        delta_j_world_units = convert(I, -1)
+        i_stop_tile_units, j_stop_tile_units, hit_dimension = RC.cast_ray(obstacle_tile_map, i_start_world_units, j_start_world_units, delta_i_world_units, delta_j_world_units, world_units_per_tile_unit)
+        Test.@test i_stop_tile_units == convert(I, 3)
+        Test.@test j_stop_tile_units == convert(I, 1)
+        Test.@test hit_dimension == 2
+    end
+
+    Test.@testset "delta_i = 1, delta_j = -2" begin
+        i_start_world_units = convert(I, height_obstacle_tile_map * world_units_per_tile_unit ÷ 2 + 1)
+        j_start_world_units = convert(I, width_obstacle_tile_map * world_units_per_tile_unit ÷ 2 + 1)
+        delta_i_world_units = convert(I, 1)
+        delta_j_world_units = convert(I, -2)
+        i_stop_tile_units, j_stop_tile_units, hit_dimension = RC.cast_ray(obstacle_tile_map, i_start_world_units, j_start_world_units, delta_i_world_units, delta_j_world_units, world_units_per_tile_unit)
+        Test.@test i_stop_tile_units == convert(I, 4)
+        Test.@test j_stop_tile_units == convert(I, 1)
+        Test.@test hit_dimension == 2
+    end
+
+    Test.@testset "delta_i = 1, delta_j = -1" begin
+        i_start_world_units = convert(I, height_obstacle_tile_map * world_units_per_tile_unit ÷ 2 + 1)
+        j_start_world_units = convert(I, width_obstacle_tile_map * world_units_per_tile_unit ÷ 2 + 1)
+        delta_i_world_units = convert(I, 1)
+        delta_j_world_units = convert(I, -1)
+        i_stop_tile_units, j_stop_tile_units, hit_dimension = RC.cast_ray(obstacle_tile_map, i_start_world_units, j_start_world_units, delta_i_world_units, delta_j_world_units, world_units_per_tile_unit)
+        Test.@test i_stop_tile_units == convert(I, 5)
+        Test.@test j_stop_tile_units == convert(I, 2)
+        Test.@test hit_dimension == 1
+    end
+
+    Test.@testset "delta_i = 2, delta_j = -1" begin
+        i_start_world_units = convert(I, height_obstacle_tile_map * world_units_per_tile_unit ÷ 2 + 1)
+        j_start_world_units = convert(I, width_obstacle_tile_map * world_units_per_tile_unit ÷ 2 + 1)
+        delta_i_world_units = convert(I, 2)
+        delta_j_world_units = convert(I, -1)
+        i_stop_tile_units, j_stop_tile_units, hit_dimension = RC.cast_ray(obstacle_tile_map, i_start_world_units, j_start_world_units, delta_i_world_units, delta_j_world_units, world_units_per_tile_unit)
+        Test.@test i_stop_tile_units == convert(I, 5)
+        Test.@test j_stop_tile_units == convert(I, 2)
+        Test.@test hit_dimension == 1
     end
 end
