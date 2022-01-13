965c26c42f5e965a5452efdefb348f62a227017f
Siddharth Bhatia <sidb60200@gmail.com>
2022-01-13 18:41:32 +0530

    rename i_start_tile_units to i_ray_start_tile and similarly for j

--- src/RayCaster.jl
+++ src/RayCaster.jl
@@ -5,8 +5,8 @@ convert_cell_to_tile(i::Integer, cells_per_tile::Integer) = (i - one(i)) ÷ cell
 function cast_ray(obstacle_tile_map::AbstractArray{Bool, 2}, i_ray_start_cell, j_ray_start_cell, i_ray_direction, j_ray_direction, cells_per_tile)
     I = typeof(i_ray_start_cell)
 
-    i_start_tile_units = convert_cell_to_tile(i_ray_start_cell, cells_per_tile)
-    j_start_tile_units = convert_cell_to_tile(j_ray_start_cell, cells_per_tile)
+    i_ray_start_tile = convert_cell_to_tile(i_ray_start_cell, cells_per_tile)
+    j_ray_start_tile = convert_cell_to_tile(j_ray_start_cell, cells_per_tile)
 
     delta_euclidean_per_world_unit_i = abs(j_ray_direction)
     delta_euclidean_per_world_unit_j = abs(i_ray_direction)
@@ -16,26 +16,26 @@ function cast_ray(obstacle_tile_map::AbstractArray{Bool, 2}, i_ray_start_cell, j
 
     if i_ray_direction < zero(I)
         delta_i_tile_units = -one(I)
-        delta_i_world_units_to_exit_start_tile = (i_ray_start_cell - (i_start_tile_units - one(I)) * cells_per_tile)
+        delta_i_world_units_to_exit_start_tile = (i_ray_start_cell - (i_ray_start_tile - one(I)) * cells_per_tile)
     else
         delta_i_tile_units = one(I)
-        delta_i_world_units_to_exit_start_tile = (i_start_tile_units * cells_per_tile - i_ray_start_cell + one(I))
+        delta_i_world_units_to_exit_start_tile = (i_ray_start_tile * cells_per_tile - i_ray_start_cell + one(I))
     end
 
     delta_euclidean_i = delta_i_world_units_to_exit_start_tile * delta_euclidean_per_world_unit_i
 
     if j_ray_direction < zero(I)
         delta_j_tile_units = -one(I)
-        delta_j_world_units_to_exit_start_tile = (j_ray_start_cell - (j_start_tile_units - one(I)) * cells_per_tile)
+        delta_j_world_units_to_exit_start_tile = (j_ray_start_cell - (j_ray_start_tile - one(I)) * cells_per_tile)
     else
         delta_j_tile_units = one(I)
-        delta_j_world_units_to_exit_start_tile = (j_start_tile_units * cells_per_tile - j_ray_start_cell + one(I))
+        delta_j_world_units_to_exit_start_tile = (j_ray_start_tile * cells_per_tile - j_ray_start_cell + one(I))
     end
 
     delta_euclidean_j = delta_j_world_units_to_exit_start_tile * delta_euclidean_per_world_unit_j
 
-    i_stop_tile_units = i_start_tile_units
-    j_stop_tile_units = j_start_tile_units
+    i_stop_tile_units = i_ray_start_tile
+    j_stop_tile_units = j_ray_start_tile
     hit_dimension = 1
 
     while !obstacle_tile_map[i_stop_tile_units, j_stop_tile_units]
@@ -52,7 +52,7 @@ function cast_ray(obstacle_tile_map::AbstractArray{Bool, 2}, i_ray_start_cell, j
 
     end
 
-    return i_start_tile_units, j_start_tile_units, i_stop_tile_units, j_stop_tile_units, hit_dimension, delta_i_world_units_to_exit_start_tile, delta_j_world_units_to_exit_start_tile
+    return i_ray_start_tile, j_ray_start_tile, i_stop_tile_units, j_stop_tile_units, hit_dimension, delta_i_world_units_to_exit_start_tile, delta_j_world_units_to_exit_start_tile
 end
 
 end
