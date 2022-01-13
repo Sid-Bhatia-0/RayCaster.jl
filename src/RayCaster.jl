5358c82a4b8f8be21321c1fa53ebda9c7fc4c432
Siddharth Bhatia <sidb60200@gmail.com>
2022-01-13 14:54:17 +0530

    remove ray_cast_continuous_world

--- src/RayCaster.jl
+++ src/RayCaster.jl
@@ -1,54 +1,5 @@
 module RayCaster
 
-function cast_ray_continous_world(obstacle_tile_map::AbstractArray{Bool, 2}, x_start::T1, y_start::T1, cos_theta::T2, sin_theta::T2) where {T1, T2}
-    i_start = floor(Int, x_start) + 1
-    j_start = floor(Int, y_start) + 1
-
-    delta_euclidean_per_unit_x = abs(1 / cos_theta)
-    delta_euclidean_per_unit_y = abs(1 / sin_theta)
-
-    hit_dimension = 1
-
-    if cos_theta < zero(T2)
-        delta_i = -1
-        delta_x_world_units_to_exit_start_tile = x_start - i_start + 1
-    else
-        delta_i = 1
-        delta_x_world_units_to_exit_start_tile = i_start - x_start
-    end
-
-    delta_euclidean_x = delta_x_world_units_to_exit_start_tile * delta_euclidean_per_unit_x
-
-    if sin_theta < zero(T2)
-        delta_j = -1
-        delta_y_world_units_to_exit_start_tile = y_start - j_start + 1
-    else
-        delta_j = 1
-        delta_y_world_units_to_exit_start_tile = j_start - y_start
-    end
-
-    delta_euclidean_y = delta_y_world_units_to_exit_start_tile * delta_euclidean_per_unit_y
-
-    i_stop = i_start
-    j_stop = j_start
-
-    while !obstacle_tile_map[i_stop, j_stop]
-
-        if (delta_euclidean_x <= delta_euclidean_y)
-            delta_euclidean_x += delta_euclidean_per_unit_x
-            i_stop += delta_i
-            hit_dimension = 1
-        else
-            delta_euclidean_y += delta_euclidean_per_unit_y
-            j_stop += delta_j
-            hit_dimension = 2
-        end
-
-    end
-
-    return i_stop, j_stop, hit_dimension, delta_x_world_units_to_exit_start_tile, delta_y_world_units_to_exit_start_tile
-end
-
 function cast_ray_discrete_world(obstacle_tile_map::AbstractArray{Bool, 2}, i_start_world_units, j_start_world_units, delta_i_world_units, delta_j_world_units, world_units_per_tile_unit)
     I = typeof(i_start_world_units)
 
