2305622e5858d08cce862b8b17caf8834d14e0ee
Siddharth Bhatia <sidb60200@gmail.com>
2022-01-16 13:01:08 +0530

    add more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -380,4 +380,205 @@ Test.@testset "RayCaster.jl" begin
             Test.@test hit_dimension == 1
         end
     end
+
+    Test.@testset "cells_per_tile_1d = 1" begin
+        cells_per_tile_1d = convert(I, 1)
+
+        i_ray_start_cell = convert(I, (height_obstacle_tile_map * cells_per_tile_1d) ÷ 2 + 1)
+        j_ray_start_cell = convert(I, (width_obstacle_tile_map * cells_per_tile_1d) ÷ 2 + 1)
+
+        Test.@testset "delta_i = 1, delta_j = 0" begin
+            i_ray_direction = convert(I, 1)
+            j_ray_direction = convert(I, 0)
+            i_ray_stop_cell, j_ray_stop_cell, i_ray_hit_tile, j_ray_hit_tile, hit_dimension = RC.cast_ray(obstacle_tile_map, cells_per_tile_1d, i_ray_start_cell, j_ray_start_cell, i_ray_direction, j_ray_direction, max_steps)
+            Test.@test i_ray_stop_cell == convert(I, 4)
+            Test.@test j_ray_stop_cell == convert(I, 3)
+            Test.@test i_ray_hit_tile == convert(I, 5)
+            Test.@test j_ray_hit_tile == convert(I, 3)
+            Test.@test hit_dimension == 1
+        end
+
+        Test.@testset "delta_i = 2, delta_j = 1" begin
+            i_ray_direction = convert(I, 2)
+            j_ray_direction = convert(I, 1)
+            i_ray_stop_cell, j_ray_stop_cell, i_ray_hit_tile, j_ray_hit_tile, hit_dimension = RC.cast_ray(obstacle_tile_map, cells_per_tile_1d, i_ray_start_cell, j_ray_start_cell, i_ray_direction, j_ray_direction, max_steps)
+            Test.@test i_ray_stop_cell == convert(I, 4)
+            Test.@test j_ray_stop_cell == convert(I, 4)
+            Test.@test i_ray_hit_tile == convert(I, 5)
+            Test.@test j_ray_hit_tile == convert(I, 4)
+            Test.@test hit_dimension == 1
+        end
+
+        Test.@testset "delta_i = 1, delta_j = 1" begin
+            i_ray_direction = convert(I, 1)
+            j_ray_direction = convert(I, 1)
+            i_ray_stop_cell, j_ray_stop_cell, i_ray_hit_tile, j_ray_hit_tile, hit_dimension = RC.cast_ray(obstacle_tile_map, cells_per_tile_1d, i_ray_start_cell, j_ray_start_cell, i_ray_direction, j_ray_direction, max_steps)
+            Test.@test i_ray_stop_cell == convert(I, 4)
+            Test.@test j_ray_stop_cell == convert(I, 4)
+            Test.@test i_ray_hit_tile == convert(I, 5)
+            Test.@test j_ray_hit_tile == convert(I, 4)
+            Test.@test hit_dimension == 1
+        end
+
+        Test.@testset "delta_i = 1, delta_j = 2" begin
+            i_ray_direction = convert(I, 1)
+            j_ray_direction = convert(I, 2)
+            i_ray_stop_cell, j_ray_stop_cell, i_ray_hit_tile, j_ray_hit_tile, hit_dimension = RC.cast_ray(obstacle_tile_map, cells_per_tile_1d, i_ray_start_cell, j_ray_start_cell, i_ray_direction, j_ray_direction, max_steps)
+            Test.@test i_ray_stop_cell == convert(I, 4)
+            Test.@test j_ray_stop_cell == convert(I, 4)
+            Test.@test i_ray_hit_tile == convert(I, 4)
+            Test.@test j_ray_hit_tile == convert(I, 5)
+            Test.@test hit_dimension == 2
+        end
+
+        Test.@testset "delta_i = 0, delta_j = 1" begin
+            i_ray_direction = convert(I, 0)
+            j_ray_direction = convert(I, 1)
+            i_ray_stop_cell, j_ray_stop_cell, i_ray_hit_tile, j_ray_hit_tile, hit_dimension = RC.cast_ray(obstacle_tile_map, cells_per_tile_1d, i_ray_start_cell, j_ray_start_cell, i_ray_direction, j_ray_direction, max_steps)
+            Test.@test i_ray_stop_cell == convert(I, 3)
+            Test.@test j_ray_stop_cell == convert(I, 4)
+            Test.@test i_ray_hit_tile == convert(I, 3)
+            Test.@test j_ray_hit_tile == convert(I, 5)
+            Test.@test hit_dimension == 2
+        end
+
+        Test.@testset "delta_i = -1, delta_j = 2" begin
+            i_ray_direction = convert(I, -1)
+            j_ray_direction = convert(I, 2)
+            i_ray_stop_cell, j_ray_stop_cell, i_ray_hit_tile, j_ray_hit_tile, hit_dimension = RC.cast_ray(obstacle_tile_map, cells_per_tile_1d, i_ray_start_cell, j_ray_start_cell, i_ray_direction, j_ray_direction, max_steps)
+            Test.@test i_ray_stop_cell == convert(I, 2)
+            Test.@test j_ray_stop_cell == convert(I, 4)
+            Test.@test i_ray_hit_tile == convert(I, 2)
+            Test.@test j_ray_hit_tile == convert(I, 5)
+            Test.@test hit_dimension == 2
+        end
+
+        Test.@testset "delta_i = -1, delta_j = 1" begin
+            i_ray_direction = convert(I, -1)
+            j_ray_direction = convert(I, 1)
+            i_ray_stop_cell, j_ray_stop_cell, i_ray_hit_tile, j_ray_hit_tile, hit_dimension = RC.cast_ray(obstacle_tile_map, cells_per_tile_1d, i_ray_start_cell, j_ray_start_cell, i_ray_direction, j_ray_direction, max_steps)
+            Test.@test i_ray_stop_cell == convert(I, 2)
+            Test.@test j_ray_stop_cell == convert(I, 4)
+            Test.@test i_ray_hit_tile == convert(I, 1)
+            Test.@test j_ray_hit_tile == convert(I, 4)
+            Test.@test hit_dimension == 1
+        end
+
+        Test.@testset "delta_i = -2, delta_j = 1" begin
+            i_ray_direction = convert(I, -2)
+            j_ray_direction = convert(I, 1)
+            i_ray_stop_cell, j_ray_stop_cell, i_ray_hit_tile, j_ray_hit_tile, hit_dimension = RC.cast_ray(obstacle_tile_map, cells_per_tile_1d, i_ray_start_cell, j_ray_start_cell, i_ray_direction, j_ray_direction, max_steps)
+            Test.@test i_ray_stop_cell == convert(I, 2)
+            Test.@test j_ray_stop_cell == convert(I, 4)
+            Test.@test i_ray_hit_tile == convert(I, 1)
+            Test.@test j_ray_hit_tile == convert(I, 4)
+            Test.@test hit_dimension == 1
+        end
+
+        Test.@testset "delta_i = -1, delta_j = 0" begin
+            i_ray_direction = convert(I, -1)
+            j_ray_direction = convert(I, 0)
+            i_ray_stop_cell, j_ray_stop_cell, i_ray_hit_tile, j_ray_hit_tile, hit_dimension = RC.cast_ray(obstacle_tile_map, cells_per_tile_1d, i_ray_start_cell, j_ray_start_cell, i_ray_direction, j_ray_direction, max_steps)
+            Test.@test i_ray_stop_cell == convert(I, 2)
+            Test.@test j_ray_stop_cell == convert(I, 3)
+            Test.@test i_ray_hit_tile == convert(I, 1)
+            Test.@test j_ray_hit_tile == convert(I, 3)
+            Test.@test hit_dimension == 1
+        end
+
+        Test.@testset "delta_i = -2, delta_j = -1" begin
+            i_ray_direction = convert(I, -2)
+            j_ray_direction = convert(I, -1)
+            i_ray_stop_cell, j_ray_stop_cell, i_ray_hit_tile, j_ray_hit_tile, hit_dimension = RC.cast_ray(obstacle_tile_map, cells_per_tile_1d, i_ray_start_cell, j_ray_start_cell, i_ray_direction, j_ray_direction, max_steps)
+            Test.@test i_ray_stop_cell == convert(I, 2)
+            Test.@test j_ray_stop_cell == convert(I, 2)
+            Test.@test i_ray_hit_tile == convert(I, 1)
+            Test.@test j_ray_hit_tile == convert(I, 2)
+            Test.@test hit_dimension == 1
+        end
+
+        Test.@testset "delta_i = -1, delta_j = -1" begin
+            i_ray_direction = convert(I, -1)
+            j_ray_direction = convert(I, -1)
+            i_ray_stop_cell, j_ray_stop_cell, i_ray_hit_tile, j_ray_hit_tile, hit_dimension = RC.cast_ray(obstacle_tile_map, cells_per_tile_1d, i_ray_start_cell, j_ray_start_cell, i_ray_direction, j_ray_direction, max_steps)
+            Test.@test i_ray_stop_cell == convert(I, 2)
+            Test.@test j_ray_stop_cell == convert(I, 2)
+            Test.@test i_ray_hit_tile == convert(I, 1)
+            Test.@test j_ray_hit_tile == convert(I, 2)
+            Test.@test hit_dimension == 1
+        end
+
+        Test.@testset "delta_i = -1, delta_j = -2" begin
+            i_ray_direction = convert(I, -1)
+            j_ray_direction = convert(I, -2)
+            i_ray_stop_cell, j_ray_stop_cell, i_ray_hit_tile, j_ray_hit_tile, hit_dimension = RC.cast_ray(obstacle_tile_map, cells_per_tile_1d, i_ray_start_cell, j_ray_start_cell, i_ray_direction, j_ray_direction, max_steps)
+            Test.@test i_ray_stop_cell == convert(I, 2)
+            Test.@test j_ray_stop_cell == convert(I, 2)
+            Test.@test i_ray_hit_tile == convert(I, 2)
+            Test.@test j_ray_hit_tile == convert(I, 1)
+            Test.@test hit_dimension == 2
+        end
+
+        Test.@testset "delta_i = 0, delta_j = -1" begin
+            i_ray_direction = convert(I, 0)
+            j_ray_direction = convert(I, -1)
+            i_ray_stop_cell, j_ray_stop_cell, i_ray_hit_tile, j_ray_hit_tile, hit_dimension = RC.cast_ray(obstacle_tile_map, cells_per_tile_1d, i_ray_start_cell, j_ray_start_cell, i_ray_direction, j_ray_direction, max_steps)
+            Test.@test i_ray_stop_cell == convert(I, 3)
+            Test.@test j_ray_stop_cell == convert(I, 2)
+            Test.@test i_ray_hit_tile == convert(I, 3)
+            Test.@test j_ray_hit_tile == convert(I, 1)
+            Test.@test hit_dimension == 2
+        end
+
+        Test.@testset "delta_i = 1, delta_j = -2" begin
+            i_ray_direction = convert(I, 1)
+            j_ray_direction = convert(I, -2)
+            i_ray_stop_cell, j_ray_stop_cell, i_ray_hit_tile, j_ray_hit_tile, hit_dimension = RC.cast_ray(obstacle_tile_map, cells_per_tile_1d, i_ray_start_cell, j_ray_start_cell, i_ray_direction, j_ray_direction, max_steps)
+            Test.@test i_ray_stop_cell == convert(I, 4)
+            Test.@test j_ray_stop_cell == convert(I, 2)
+            Test.@test i_ray_hit_tile == convert(I, 4)
+            Test.@test j_ray_hit_tile == convert(I, 1)
+            Test.@test hit_dimension == 2
+        end
+
+        Test.@testset "delta_i = 1, delta_j = -1" begin
+            i_ray_direction = convert(I, 1)
+            j_ray_direction = convert(I, -1)
+            i_ray_stop_cell, j_ray_stop_cell, i_ray_hit_tile, j_ray_hit_tile, hit_dimension = RC.cast_ray(obstacle_tile_map, cells_per_tile_1d, i_ray_start_cell, j_ray_start_cell, i_ray_direction, j_ray_direction, max_steps)
+            Test.@test i_ray_stop_cell == convert(I, 4)
+            Test.@test j_ray_stop_cell == convert(I, 2)
+            Test.@test i_ray_hit_tile == convert(I, 5)
+            Test.@test j_ray_hit_tile == convert(I, 2)
+            Test.@test hit_dimension == 1
+        end
+
+        Test.@testset "delta_i = 2, delta_j = -1" begin
+            i_ray_direction = convert(I, 2)
+            j_ray_direction = convert(I, -1)
+            i_ray_stop_cell, j_ray_stop_cell, i_ray_hit_tile, j_ray_hit_tile, hit_dimension = RC.cast_ray(obstacle_tile_map, cells_per_tile_1d, i_ray_start_cell, j_ray_start_cell, i_ray_direction, j_ray_direction, max_steps)
+            Test.@test i_ray_stop_cell == convert(I, 4)
+            Test.@test j_ray_stop_cell == convert(I, 2)
+            Test.@test i_ray_hit_tile == convert(I, 5)
+            Test.@test j_ray_hit_tile == convert(I, 2)
+            Test.@test hit_dimension == 1
+        end
+    end
+
+    Test.@testset "starting inside an obstacle tile" begin
+        cells_per_tile_1d = convert(I, 8)
+
+        i_ray_start_cell = convert(I, 3)
+        j_ray_start_cell = convert(I, 4)
+
+        Test.@testset "delta_i = 1, delta_j = 0" begin
+            i_ray_direction = convert(I, 1)
+            j_ray_direction = convert(I, 0)
+            i_ray_stop_cell, j_ray_stop_cell, i_ray_hit_tile, j_ray_hit_tile, hit_dimension = RC.cast_ray(obstacle_tile_map, cells_per_tile_1d, i_ray_start_cell, j_ray_start_cell, i_ray_direction, j_ray_direction, max_steps)
+            Test.@test i_ray_stop_cell == convert(I, 3)
+            Test.@test j_ray_stop_cell == convert(I, 4)
+            Test.@test i_ray_hit_tile == convert(I, 1)
+            Test.@test j_ray_hit_tile == convert(I, 1)
+            Test.@test hit_dimension == 0
+        end
+    end
 end
